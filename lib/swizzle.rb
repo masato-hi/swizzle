e3be835ca86459804ae568aa1df551c0d232ab21
masato <masato.hirahata@gmail.com>
2017-08-26 04:33:45 +0900

    initizalize

---
+++ lib/swizzle.rb
@@ -0,0 +1,5 @@
+require "swizzle/version"
+
+module Swizzle
+  # Your code goes here...
+end
---
+++ test/swizzle_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class SwizzleTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Swizzle::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
